f69f239fc1cc18e9386aa5cf577de790d8aa80ca
Pan GaoYong <pan.gaoyong@gmail.com>
2017-03-10 15:05:16 +0800

    cam http client rubocop'ed

--- lib/camdict/http_client.rb
+++ lib/camdict/http_client.rb
@@ -1,22 +1,21 @@
+# frozen_string_literal: true
 module Camdict
+  # HTTP module
   module HTTP
-
-    require "open-uri"
+    require 'open-uri'
 
     # A default user agent string for this http client. It can be customised.
-    AGENT = 
-      "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"
+    AGENT =
+      'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0'
 
+    # HTTP Client class
     class Client
-
       # Download a html page from a remote site, and return a Nokogiri::HTML
       # +url+ will be escaped by this method, and default +agtstr+ is AGENT.
-      def self.get_html(url, agtstr=AGENT)
+      def self.get_html(url, agtstr = AGENT)
         url = URI.escape(url)
-        Nokogiri::HTML(open(url, "User-Agent"=>agtstr))
+        Nokogiri::HTML(open(url, 'User-Agent' => agtstr))
       end
-
     end
-
   end
 end
--- test/test_http_client.rb
+++ test/test_http_client.rb
@@ -1,28 +1,37 @@
+# frozen_string_literal: true
 require 'minitest/autorun'
 require 'camdict'
 
 module Camdict
   class HTTPClientTest < Minitest::Test
-    
     def test_get_html
       require 'webrick'
 
-      server = WEBrick::HTTPServer.new(:Port=>0, :BindAddress=>"127.0.0.1", 
-        :Logger=>WEBrick::Log.new(nil,WEBrick::BasicLog::FATAL))
-      Thread.new {
-        res = Proc.new { |r, q|
-          q.body ="hello"
-        }
-        server.mount_proc '/hi', nil, &res
-        server.start
-      }
-      Thread.new {
-        url = "http://127.0.0.1:#{server.config[:Port]}/hi"
-        page = Camdict::HTTP::Client.get_html(url)
-        server.stop 
-        assert_equal "hello", page.text
-      }.join
+      server = WEBrick::HTTPServer.new(
+        Port: 0, BindAddress: '127.0.0.1',
+        Logger: WEBrick::Log.new(nil, WEBrick::BasicLog::FATAL)
+      )
+      start_hello_thread(server)
+      http_hello(server)
     end
 
+    def start_hello_thread(server)
+      Thread.new do
+        res = proc do |_r, q|
+          q.body = 'hello'
+        end
+        server.mount_proc '/hi', nil, &res
+        server.start
+      end
+    end
+
+    def http_hello(server)
+      Thread.new do
+        url = "http://127.0.0.1:#{server.config[:Port]}/hi"
+        page = Camdict::HTTP::Client.get_html(url)
+        server.stop
+        assert_equal 'hello', page.text
+      end.join
+    end
   end
 end
