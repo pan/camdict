87219a89a7f9f7425f57fef727fba1a00474e948
Pan GaoYong <pan.gaoyong@gmail.com>
2017-03-27 11:38:33 +0800

    a better straightforward word lookup API

--- lib/camdict/word.rb
+++ lib/camdict/word.rb
@@ -1,36 +1,102 @@
+# frozen_string_literal: true
 require 'camdict/client'
 require 'camdict/definition'
 
 module Camdict
-  # Get all definitions data about a word or phrase including IPAs, 
-  # pronunciations, usage sentences, etc. from Camdict Client.
+  # Get all definitions data about a word or phrase including IPAs,
+  # pronunciations, usage sentences, etc. from Cambridge dictionary.
   class Word
-
     # New a +word+ or phrase, default +dictionary+ is british.
-    def initialize(word, dictionary=nil)
-      @word ||= word
+    def initialize(word, dictionary = nil)
+      @word = word
       @dictionary = dictionary
-      @raw_definitions = []   # each element is a hash
-      @definitions = []       # each element is a Definition object
+    end
+
+    def part_of_speech
+      s = definition.senses.map(&:part_of_speech).uniq
+      return s.first if s.count < 2
+      s
+    end
+
+    def pronunciation(region = :uk)
+      p = definition.pronunciation.send(region)
+      p.mp3 || p.ogg
+    end
+
+    def ipa(region = :uk)
+      definition.ipa.send(region)
+    end
+
+    def meaning
+      definition.senses.first.explanations.first.meaning
+    end
+
+    def meanings
+      definition.senses.map { |s| s.explanations.map(&:meaning) }.flatten
+    end
+
+    # show all important dictionary information, returns
+    # { meaning: [{ pos: '', category: '', level: '', code: '',
+    #             sense: [{ meaning:, eg: [] }] }]
+    #   ipa: '' | { uk: , us: },
+    #   pronunciation: { uk: mp3|ogg, us: mp3|ogg }
+    # }
+    def show
+      {
+        meaning: meanings_json,
+        ipa: ipa_json,
+        pronunciation: { uk: pronunciation, us: pronunciation(:us) }
+      }
+    end
+
+    def print
+      require 'pp'
+      pp show
+    end
+
+    def definition
+      definitions.first
     end
 
     # Get all definitions for this word from remote online dictionary
     def definitions
-      client = Camdict::Client.new(@dictionary)
-      @raw_definitions = client.html_definition(@word)
-      if found?
-        @definitions = @raw_definitions.map { |r|
-          Camdict::Definition.new(@word, r)
+      @definitions ||= g_definitions
+    end
+
+    def raw_definitions
+      @raw_definitions ||= retrieve.map do |r|
+        r.values.first.to_html(save_with: 0)
+      end
+    end
+
+    alias pos part_of_speech
+
+    private
+
+    def retrieve
+      @retrieved ||= Camdict::Client.new(@dictionary).html_definition(@word)
+    end
+
+    def g_definitions
+      retrieve.map do |r|
+        hdef = r.values.first
+        Camdict::Definition.new(@word).parse(hdef)
+      end
+    end
+
+    def meanings_json
+      definition.senses.map do |s|
+        {
+          pos: s.part_of_speech, category: s.category,
+          sense: s.explanations.map do |e|
+            { meaning: e.meaning, eg: e.examples&.map(&:sentence) }
+          end
         }
       end
     end
 
-    # Found in the diciontary? Return number of found entries
-    def found?
-      @raw_definitions.size
+    def ipa_json
+      ipa(:uk) == ipa(:us) ? ipa : { uk: ipa(:uk), us: ipa(:us) }
     end
-
-    alias in? found?
-
   end
 end
---
+++ test/itest_word.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+require_relative 'helper'
+
+module Camdict
+  class WordiTest < Minitest::Test
+    def setup
+      @film = Camdict::Word.new('film')
+    end
+
+    def test_definitions
+      assert @film.definitions.size.positive?
+    end
+
+    def test_raw_definitions
+      assert @film.raw_definitions.size.positive?
+    end
+
+    def test_ipa
+      assert_equal 'fɪlm', @film.ipa
+      assert_equal 'fɪlm', @film.ipa(:us)
+    end
+
+    def test_meaning
+      m = 'a series of moving pictures, usually shown in a cinema or on' \
+        ' television and often telling a story: '
+      assert_equal m, @film.meaning
+    end
+
+    def test_meanings
+      m = 'to record moving pictures with a camera, usually to make a film' \
+        ' for television or the cinema: '
+      assert_equal m, @film.meanings.last
+      assert_equal 4, @film.meanings.size
+    end
+
+    def test_pronunciation
+      uk_mp3 = 'http://dictionary.cambridge.org/media/english/uk_pron/u/ukf/' \
+        'ukfil/ukfill_007.mp3'
+      us_mp3 = 'http://dictionary.cambridge.org/media/english/us_pron/f/fil/' \
+        'film_/film.mp3'
+      assert_equal uk_mp3, @film.pronunciation
+      assert_equal us_mp3, @film.pronunciation(:us)
+    end
+
+    def test_part_of_speech
+      assert_equal %w(noun verb), @film.part_of_speech
+    end
+  end
+end
