bce9b4174d5f0ec1c42199feec601d4e604c83de
Pan GaoYong <pan.gaoyong@gmail.com>
2017-03-28 09:41:33 +0800

    test public api of class explanation

--- test/test_explanation.rb
+++ test/test_explanation.rb
@@ -1,25 +1,24 @@
-require 'minitest/autorun'
-require 'camdict'
+# frozen_string_literal: true
+require_relative 'helper'
 
 module Camdict
   class ExplanationTest < Minitest::Test
-
     def test_get_level
       html = '<span class="def-info"><span class="epp-xref B1">B1</span>'
       html = Nokogiri::HTML(html)
       exp  = Camdict::Explanation.new(html)
-      assert_equal 'B1', (exp.send :get_level)
+      assert_equal 'B1', exp.level
     end
 
     def test_get_meaning
-      html = '<span class="def">in <a class="query" href="http://cambridge' +
-        '.org/british">agreement </a>with the true facts or with what is ' +
-        'generally accepted:'
+      html = '<span class="def">in <a class="query" href="http://cambridge' \
+             '.org/british">agreement </a>with the true facts or with what ' \
+             'is generally accepted:'
       html = Nokogiri::HTML(html)
       exp  = Camdict::Explanation.new(html)
-      expected = 
+      expected =
         'in agreement with the true facts or with what is generally accepted:'
-      assert_equal expected, (exp.send :get_meaning)
+      assert_equal expected, exp.meaning
     end
 
     def test_gc
@@ -33,39 +32,36 @@ module Camdict
     def test_get_sentence
       html = '<span class="eg">a correct answer</span>'
       html = Nokogiri::HTML(html)
-      exp  = Camdict::Explanation::Sentence.new(html)
-      assert_equal 'a correct answer', (exp.send :get_sentence)
+      exp  = Camdict::Sentence.new(html)
+      assert_equal 'a correct answer', exp.sentence
     end
 
     def test_get_examples
-      sent1 = %q(It's not correct to describe them as 'student')
-      sent2 = %q("Your name is Angela Black?""That is correct.")
+      sent1 = %(It's not correct to describe them as 'student')
+      sent2 = %("Your name is Angela Black?""That is correct.")
       html  = "<span class='examp'><span class='eg'>#{sent1}</span></span>"
-      html += "<span class='examp'><span class='eg'>#{sent2}</span>" +
-        '<span class="usage">formal</span>'
+      html += "<span class='examp'><span class='eg'>#{sent2}</span>" \
+              '<span class="usage">formal</span>'
       html = Nokogiri::HTML(html)
-      exp  = Camdict::Explanation.new(html)
-      expected = exp.send :get_examples
-      e1, e2 = expected.flatten
+      e1, e2 = Camdict::Explanation.new(html).examples
       assert_equal sent1, e1.sentence
       assert_equal sent2, e2.sentence
-      assert_equal "formal", e2.usage
+      assert_equal 'formal', e2.usage
     end
 
     def test_get_synonym
       html = '<span class="entry-xref" type="Synonym"><span class="x-h">right'
       html = Nokogiri::HTML(html)
       exp  = Camdict::Explanation.new(html)
-      assert_equal "right", (exp.send :get_synonym)
+      assert_equal 'right', exp.synonym
     end
 
     def test_get_opposite
-      html = 
+      html =
         '<span class="entry-xref" type="Opposite"><span class="x-h">incorrect'
       html = Nokogiri::HTML(html)
       exp  = Camdict::Explanation.new(html)
-      assert_equal "incorrect", (exp.send :get_opposite)
+      assert_equal 'incorrect', exp.opposite
     end
-
   end
 end
